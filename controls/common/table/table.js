231c44a4c7307068aa28b01794c83408ec8a676e
ossmcloud <development@ossmcloud.ie>
2023-09-01 22:26:09 +0100

    matching :: work in progress

--- controls/common/table/table.js
+++ controls/common/table/table.js
@@ -31,6 +31,9 @@ class TableColumn {
     #nullText = '';
     #undefinedText = '';
     #addTotals = false;
+    #link = null;
+    #toolTip = null;
+    #headerToolTip = '';
     constructor(options) {
         if (!options) { options = {}; }
         if (options.constructor.name == 'String') {
@@ -52,6 +55,9 @@ class TableColumn {
         this.#formatNumber = options.formatNumber;
         this.#unbound = options.unbound;
         this.#style = options.style || '';
+        this.#link = options.link || null;
+        this.#toolTip = options.toolTip || null;
+        this.#headerToolTip = options.headerToolTip || '';
 
         this.#nullText = (options.nullText === undefined) ? '[NULL]' : options.nullText;
         this.#undefinedText = options.undefinedText || '[UNKNOWN]';
@@ -74,7 +80,9 @@ class TableColumn {
     get unbound() { return this.#unbound; }
     get style() { return this.#style; }
     get addTotals() { return this.#addTotals; }
-
+    get link() { return this.#link; }
+    get toolTip() { return this.#toolTip; }
+    get headerToolTip() { return this.#headerToolTip; }
 
     value(object, raw) {
         var val = object[this.name];
@@ -166,10 +174,10 @@ function renderTableHeader(objects, options, tableTotals) {
         var dataFieldName = `data-field-name="${col.name}"`;
         var sortableClass = (options.sortable) ? ' class="cx_sortable"' : '';
         var textAlign = ` style="text-align: ${col.align};"`;
-        tHead += '<th ' + dataFieldName + sortableClass + textAlign + '>';
+        tHead += '<th ' + dataFieldName + sortableClass + textAlign + 'title="' + col.headerToolTip + '">';
         if (col.addTotals) {
             tableTotals[col.name] = 0;
-            tHead += '<span class="jx-col-total"><span class="jx-col-total-lbl" title="The total displayed here are relevant to the page displayed and not the overall results total">&#x1F6C8;</span><span class="jx-col-total-val">{$' + col.name +'}</span></span>';
+            tHead += '<span class="jx-col-total"><span class="jx-col-total-lbl" title="The total displayed here are relevant to the page displayed and not the overall results total">&#x1F6C8;</span><span class="jx-col-total-val">{$' + col.name + '}</span></span>';
             tHead += '<span style="display: block">' + col.title + '</span>';
         } else {
             tHead += col.title;
@@ -250,23 +258,28 @@ function renderTableBody(objects, options, tableTotals, rowTemplate) {
                 dataAttr += ` data-${col.dataHidden}="${col.value(objects[i], true)}"`;
                 continue;
             }
-            
+
+            var cellToolTip = '';
             var cellValue = col.value(objects[i]);
             if (col.addTotals) {
                 if (!tableTotals[col.name]) { tableTotals[col.name] = 0; }
                 var cellValueNo = parseFloat(objects[i][col.name]);
                 if (!isNaN(cellValueNo)) { tableTotals[col.name] += cellValueNo };
             }
+
+
             if (col.type == 'check') {
+                // CHECK: built in check box column
                 cellValue = `<input type="checkbox" style="margin: 0px; width: 30px;">`;
 
             } else if (col.input) {
+                // INPUT: the column has an input control 
                 col.input.id = 'cxlist_' + options.id + '_' + col.name + '_' + ((rowTemplate) ? 'tmpl_idx' : i);
                 col.input.name = col.input.id;
-                
+
                 col.input.fieldName = col.input.id;
                 col.input.fieldNameDb = col.name;
-                
+
                 col.input.value = objects[i][col.name];
                 col.input.dataAttributes = null;
                 col.input.data = null;
@@ -275,39 +288,62 @@ function renderTableBody(objects, options, tableTotals, rowTemplate) {
                 cellColStyle += ' padding: 1px 0px 0px 0px;';
 
             } else {
-                if (col.name == options.primaryKey) {
-                    if (options.path) {
-                        var target = ' target="' + (options.linkTarget || '_self') + '" ';
-                        var link = options.path + ((options.path.indexOf('?') < 0) ? '?' : '&') + 'id=' + cellValue;
-                        cellValue = '<a tabindex="-1" style="text-decoration: none;"' + target + 'href="' + link + '" title="view...">&#128269;</a>';
-                        if (options.allowEditCondition) {
-                            if (options.allowEditCondition(objects[i])) { cellValue += ' <a tabindex="-1" style="text-decoration: none;" ' + target + 'href="' + link + '&e=T" title="edit...">&#x270E;</a>'; }
-                        } else if (options.allowEdit) {
-                            cellValue += ' <a tabindex="-1" style="text-decoration: none;" ' + target + 'href="' + link + '&e=T" title="edit...">&#x270E;</a>';
-                        }
-                        col.width = '30px';
+                if (options.path && col.name == options.primaryKey) {
+                    // PRIMARY KEY VIEW EDIT LINKS: we only show this if we have a .path set
+                    var target = ' target="' + (options.linkTarget || '_self') + '" ';
+                    var link = options.path + ((options.path.indexOf('?') < 0) ? '?' : '&') + 'id=' + cellValue;
+                    cellValue = '<a tabindex="-1" style="text-decoration: none;"' + target + 'href="' + link + '" title="view...">&#128269;</a>';
+                    if (options.allowEditCondition) {
+                        if (options.allowEditCondition(objects[i])) { cellValue += ' <a tabindex="-1" style="text-decoration: none;" ' + target + 'href="' + link + '&e=T" title="edit...">&#x270E;</a>'; }
+                    } else if (options.allowEdit) {
+                        cellValue += ' <a tabindex="-1" style="text-decoration: none;" ' + target + 'href="' + link + '&e=T" title="edit...">&#x270E;</a>';
                     }
+                    col.width = '30px';
+
                 } else {
+                    // BOOLEAN RENDER: Replace boolean values with 'checked' / 'unchecked' UTF-* chars
                     if (cellValue === false) { cellValue = '&#x2610;'; }
                     if (cellValue === true) { cellValue = '&#x2611;'; }
-                    if (objects[i].style) {
-                        cellValue = '<span style="' + objects[i].style + '">' + cellValue + '</span>';
+
+                    // CUSTOM LINK:
+                    if (col.link) {
+                        var linkValue = col.value(objects[i]);
+                        // @@IMPROVE: we could accept valueField as array of valueField, paramName for the url
+                        if (col.link.valueField) { linkValue = objects[i][col.link.valueField]; }
+                        if (linkValue) {
+                            var linkPlaceHolder = '{' + (col.link.paramName || col.name) + '}';
+                            var linkUrl = (col.link.constructor.name == 'String') ? col.link : col.link.url;
+                            linkUrl = linkUrl.replace(linkPlaceHolder, linkValue);
+                            cellValue = `<a href="${linkUrl}" target="_blank" >${cellValue}</a>`;
+                        }
+
                     }
                 }
+
+                if (col.toolTip) {
+                    cellToolTip = objects[i][col.toolTip.valueField || col.name] || '';
+                    if (col.toolTip.suppressText) { cellValue = ''; }
+                }
+
+                // CUSTOM STYLE: if a .style is passed then we wrap the value with it 
+                if (objects[i].style) { cellValue = '<span style="' + objects[i].style + '">' + cellValue + '</span>'; }
             }
 
-            //
+            // now get cell highlight styles and wrap if any
             var cellStyle = getCellHighlightStyle(objects[i], col, options);
             if (cellStyle) { cellValue = `<span style="${cellStyle}">${cellValue}</span>`; }
 
+            // NOTE: if we have list actions and this is the 1st visible cell, 
+            //          then we add an hidden input control with the line's id so that actions can safely and efficiently retrieve it
             if (options.listActions && jj == 0) {
-                var hiddenId = 'cxlist_' +  options.id + '_lineId_' + ((rowTemplate) ? 'tmpl_idx' : i);
+                var hiddenId = 'cxlist_' + options.id + '_lineId_' + ((rowTemplate) ? 'tmpl_idx' : i);
                 cellValue += `<input type="hidden" id="${hiddenId}" name="${hiddenId}" value="${objects[i].id}">`;
             }
             jj++;
-            
-            tRow += `<td style="width: ${col.width}; text-align: ${col.align}; ${(col.fontSize ? 'font-size: ' + col.fontSize + ';' : '')} ${cellColStyle}">${cellValue}</td>`;
-            
+
+            // build table cell
+            tRow += `<td style="width: ${col.width}; text-align: ${col.align}; ${(col.fontSize ? 'font-size: ' + col.fontSize + ';' : '')} ${cellColStyle}" title="${cellToolTip}">${cellValue}</td>`;
+
         }
 
         tRow = tRow.replace('[$DATA$]', dataAttr);
@@ -343,6 +379,7 @@ function getHighlightStyle(object, options) {
             style += (h.customStyle(object, rawVal, h) || '');
         }
         if (style) { style += ' '; }
+        
     }
     return style;
 }
@@ -360,23 +397,30 @@ function getCellHighlightStyle(object, column, options) {
         //     continue;
         // }
 
+        var applied = false;
         var rawVal = object[h.column];
         if (h.op == '=') {
-            if (rawVal == h.value) { style += h.style }
+            if (rawVal == h.value) { style += h.style; applied = true; }
         } else if (h.op == '!=') {
-            if (rawVal != h.value) { style += h.style }
+            if (rawVal != h.value) { style += h.style; applied = true; }
         } else if (h.op == '>') {
-            if (rawVal > h.value) { style += h.style; }
+            if (rawVal > h.value) { style += h.style; applied = true; }
         } else if (h.op == '>=') {
-            if (rawVal >= h.value) { style += h.style; }
+            if (rawVal >= h.value) { style += h.style; applied = true; }
         } else if (h.op == '<') {
-            if (rawVal < h.value) { style += h.style; }
+            if (rawVal < h.value) { style += h.style; applied = true; }
         } else if (h.op == '<=') {
-            if (rawVal <= h.value) { style += h.style; }
+            if (rawVal <= h.value) { style += h.style; applied = true; }
         } else if (h.customStyle) {
-            style += (h.customStyle(object, rawVal, h) || '');
+            var tStyle = (h.customStyle(object, rawVal, h) || '');
+            if (tStyle) {
+                applied = true;
+                style += tStyle;
+            }
+
         }
         if (style) { style += ' '; }
+        if (h.stop && applied) { break; }
     }
     return style;
 }
